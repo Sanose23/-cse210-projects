d918d993f4c20a328f30056f2f6d66e9838b0bf1
Sanjose23 <lawaljoseph23@gmail.com>
2024-05-01 03:59:39 +0100

    prep 3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+        int guess = 0;
+
+        Console.WriteLine("Welcome to the Guess My Number game!");
+        Console.WriteLine("I have picked a number between 1 and 100.");
+        Console.WriteLine("Try to guess it!");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Enter your guess: ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+        Console.WriteLine("Thanks for playing! Goodbye!");
     }
 }
